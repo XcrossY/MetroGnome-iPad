70d8e89e1413c8d1871b6afddf96f78d2911e89b
Alexander Pease <AlexanderPease@dynamic-oit-vapornet-c-3764.princeton.edu>
2012-01-27 21:22:02 -0500

    Upload to github

--- Classes/Views/MGSheetMusicView.m
+++ Classes/Views/MGSheetMusicView.m
@@ -23,30 +23,6 @@
     return self;
 }
 
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)orientation
-{
-    if ((orientation == UIInterfaceOrientationLandscapeRight) ||
-        (orientation == UIInterfaceOrientationLandscapeLeft))
-        return YES;
-    
-    return NO;
-}
-
-- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
-    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
-    if(toInterfaceOrientation == UIInterfaceOrientationPortrait || toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown){
-        //self.view = portraitView;
-        [self changeTheViewToPortrait:NO andDuration:duration];
-        
-    }
-    else if(toInterfaceOrientation == UIInterfaceOrientationLandscapeRight || toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft){
-        //self.view = landscapeView;
-        [self changeTheViewToPortrait:NO andDuration:duration];
-    }
-}
-
-
 -(void)display:(MGPart *)score {
     
 }
